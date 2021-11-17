fd4117c463a0a81c12971a1421bb5398742e471f
Erik Luebbers <erluebbers@gmail.com>
2021-11-17 06:30:45 -0800

    commit tree c3ac9e9819326e729902795d9e631ba236c5f66d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+
+function submitData (inputName, inputEmail) {
+    return fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: inputName,
+            email: inputEmail
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const p = document.createElement('p')
+        document.body.appendChild(p)
+        p.innerHTML = data.id
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error
+    })
+}
